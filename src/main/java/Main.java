32d7675db66b5fed4b7f9436be041b8c51a51f09
saricaBugrahan <saricabugrahan@gmail.com>
2024-03-25 21:29:32 +0300

    pass 1st stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,6 +19,7 @@ public class Main {
        System.out.println("accepted new connection");
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
+
      }
   }
 }
